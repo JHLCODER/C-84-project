b12fb687c09b445bb464a4210315932fc7b68d05
JHLCODER <79390831+JHLCODER@users.noreply.github.com>
2022-08-18 12:51:55 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-canvas=document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext('2d');
 
 img_width = 300;
 img_height = 100;
@@ -10,9 +10,9 @@ img_x = 100;
 img_y = 100;
 
 function add() {
-	img_imgTag = new Image(); 
-	img_imgTag.onload = uploadimg; 
-	img_imgTag.src = img_image; 
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
 
 function uploadimg() {
@@ -20,7 +20,7 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-window.addEventListener("keydown",my_keydown);
+window.addEventListener("keydown" , my_keydown);
 
 function my_keydown(e)
 {
@@ -30,35 +30,7 @@ function my_keydown(e)
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
 		aplhabetkey();
 		document.getElementById("d1").innerHTML="You pressed alphabet key";
-		console.log("Alphabet key");
-	else{
-		otherkey();
-		document.getElementById("d1").innerHTML="You pressed symbol or other key";
-	}
-}
-function my_keydown(e)
-{
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-	
-		if((keyPressed >=48 && keyPressed<=57)|| (keyPressed >=65 && keyPressed<=90)){
-		numberkey();
-		document.getElementById("d1").innerHTML="You pressed number key";
-		console.log("Number key");
-	else{
-		otherkey();
-		document.getElementById("d1").innerHTML="You pressed symbol or other key";
-	}
-}
-function my_keydown(e)
-{
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-	
-		if((keyPressed >=37 && keyPressed<=40)|| (keyPressed >=65 && keyPressed<=90)){
-		arrowkey();
-		document.getElementById("d1").innerHTML="You pressed arrow key";
-		console.log("Arrow key");
+		console.log("alphabet key");}
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
@@ -67,26 +39,28 @@ function my_keydown(e)
 
 function aplhabetkey()
 {
-	img_image="Alpkey.png";
+	img_image = "Alpkey.png";
 	add();
 
 }
 function numberkey()
 {
-	img_image="numkey.png";
-	add();
+ img_image = "numkey.png";
+ add();	
 }
 function arrowkey()
 {
-	img_image="arrkey.png";
+	img_image = "Arrkey.png";
 	add();
 }
 function specialkey()
 {
-	
+	img_image = "spkey.png";
+	add();
 }
 function otherkey()
 {
 	img_image="otherkey.png";
 	add();
 }
+	
